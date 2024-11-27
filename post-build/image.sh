06440a9e1d5f007ef6508978c5685ec2264335e7
Mauro (mdrjr) Ribeiro <mauro.ribeiro@hardkernel.com>
2024-11-27 16:11:12 -0300

    ODROID-M2 Initial support

--- post-build/image.sh
+++ post-build/image.sh
@@ -7,12 +7,13 @@ rm -rf /tmp/mnt
 mkdir /tmp/mnt
 
 # mount image first partition 
-sudo mount -o loop,offset=$[2048*512] build/tmp/deploy/images/odroid-m1/odroid-image-odroid-m1.rootfs.wic /tmp/mnt
+sudo mount -o loop,offset=$[2048*512] build/tmp/deploy/images/$MACHINE/odroid-image-$MACHINE.rootfs.wic /tmp/mnt
 sudo cp post-build/files/$MACHINE/boot.scr /tmp/mnt
 sudo cp post-build/files/autostart.cfg /tmp/mnt
 sudo umount /tmp/mnt
+sudo rm -rf /tmp/mnt
 
-ln -s build/tmp/deploy/images/odroid-m1/odroid-image-odroid-m1.rootfs.wic ./odroid-m1.img
+ln -s build/tmp/deploy/images/$MACHINE/odroid-image-$MACHINE.rootfs.wic ./odroid.img
 
 
 
--- pre-build/init-repo.sh
+++ pre-build/init-repo.sh
@@ -2,6 +2,24 @@
 
 git submodule update --init --recursive
 
+export BOARD="odroid-m1"
+
+echo
+echo
+echo "Select board to build:"
+echo "1 - ODROID-M1/M1S"
+echo "2 - ODROID-M2"
+echo -n ": "
+read brd
+if [ "$brd" -eq "1" ]; then
+    sed -i "s/@BOARD@/odroid-m1/g" build/conf/local.conf
+elif [ "$brd" -eq "2" ]; then
+    sed -i "s/@BOARD@/odroid-m1/g" build/conf/local.conf
+else
+    echo "ERROR: Invalid board, try again"
+    exit 0
+fi
+
 echo "Now you can run: "
 echo "source poky/oe-init-build-env"
 echo
@@ -14,6 +32,6 @@ echo
 echo
 echo "After the image is build run:"
 echo "source post-build/image.sh"
-echo "You will have the odroid-m1.img on this folder"
+echo "You will have the odroid.img on this folder"
 
 
