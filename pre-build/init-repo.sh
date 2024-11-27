0bdb7c19feafe638466b883628e41523a4391d66
Mauro (mdrjr) Ribeiro <mauro.ribeiro@hardkernel.com>
2024-11-27 01:09:42 -0300

    add init-repo with instructions

--- pre-build/init-repo.sh
+++ pre-build/init-repo.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 git submodule update --init --recursive
+
+echo "Now you can run: "
+echo "source poky/oe-init-build-env"
+echo
+echo "autostart.cfg: post-build/files/autostart.cfg"
+echo
+echo
+echo "To build a image run:"
+echo "bitbake odroid-image"
+echo
+echo
+echo "After the image is build run:"
+echo "source post-build/image.sh"
+echo "You will have the odroid-m1.img on this folder"
+
+
