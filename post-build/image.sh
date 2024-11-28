85c7fc2c6844e136de29be1849f3a6e76761d9a9
Mauro (mdrjr) Ribeiro <mauro.ribeiro@hardkernel.com>
2024-11-28 20:11:21 -0300

    Fix the post build script, right now it just creates a nice symlink

--- post-build/image.sh
+++ post-build/image.sh
@@ -2,6 +2,7 @@
 
 # Get Machine
 bitbake -e | grep MACHINE= | head -n1 > /tmp/a.sh
+source /tmp/a.sh
 
 ln -s build/tmp/deploy/images/$MACHINE/odroid-image-$MACHINE.rootfs.wic "./$MACHINE.img"
 ln -s build/tmp/deploy/images/$MACHINE/odroid-image-$MACHINE.rootfs.wic.xz "./$MACHINE.img.xz"
